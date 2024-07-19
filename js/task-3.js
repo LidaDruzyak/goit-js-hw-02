c84135f5d2ab45841e53e5c9271835ff7f1c690c
LidaDruzyak <lida.druzyak.90@gmail.com>
2024-07-19 14:13:57 +0300

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,21 @@
 'use strict';
-function getElementWidth(content, padding, border) {
-const contentWidth = parseFloat(content);
-const paddingWidth = parseFloat(padding) * 2;
-const borderWidth = parseFloat(border) * 2;
-const totalWidth = contentWidth + paddingWidth + borderWidth;
-return totalWidth;
+function checkForSpam(message) {
+    let hasSpam = message.toLowerCase();
+if(hasSpam.includes('spam')||hasSpam.includes('sale')) {
+    return true;
+}
+return false;
+
 }
 
 
 
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
